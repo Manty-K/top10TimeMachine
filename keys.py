d2d6df754aa01d2e1299837f997987c44ee17e4f
Manty-K <manthankhandale@gmail.com>
2021-02-17 20:17:29 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
-spotify_client_id = '8b36d9744d8a45c5987aee66dbdff732'
+spotify_client_id = '{insert client id here}'
 
-spotify_client_secret = '2b8e014f484b4b239556d83d35aacee0'
+spotify_client_secret = '{insert client secret here}'
 
-spotify_redirect_uri = 'http://example.com/callback/'
+spotify_redirect_uri = '{insert spotify redirect url here}'
